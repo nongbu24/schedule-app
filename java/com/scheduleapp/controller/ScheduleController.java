4acdcc4a8209360eabf4e76ce231069693c61117
nongbu24 <pop524@naver.com>
2026-04-12 21:13:06 +0900

    Feat. Lv 2 작성

--- java/com/scheduleapp/controller/ScheduleController.java
+++ java/com/scheduleapp/controller/ScheduleController.java
@@ -2,13 +2,14 @@ package com.scheduleapp.controller;
 
 import com.scheduleapp.dto.CreateScheduleRequest;
 import com.scheduleapp.dto.CreateScheduleResponse;
+import com.scheduleapp.dto.GetScheduleResponse;
 import com.scheduleapp.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,5 +20,19 @@ public class ScheduleController {
     @PostMapping("/schedules")
     public ResponseEntity<CreateScheduleResponse> createSchedule(@RequestBody CreateScheduleRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(scheduleService.save(request));
+    }
+
+    // 전체 조회
+    @GetMapping("/schedules")
+    public ResponseEntity<List<GetScheduleResponse>> getSchedules(
+            @RequestParam(required = false) String writer
+    ) {
+        return ResponseEntity.status(HttpStatus.OK).body(scheduleService.findAll(writer));
+    }
+
+    // 단 건 조회
+    @GetMapping("/schedules/{scheduleId}")
+    public ResponseEntity<GetScheduleResponse> getSchedule(@PathVariable Long scheduleId) {
+        return ResponseEntity.status(HttpStatus.OK).body(scheduleService.findOne(scheduleId));
     }
 }
